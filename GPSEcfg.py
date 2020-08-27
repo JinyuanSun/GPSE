1e917afbccb68e7b9a12b350235174d8051846a4
jysun <1650949260@qq.com>
2020-08-27 09:26:40 +0800

    update

---
+++ GPSEcfg.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+ADT_PATH = "~/MGLTools/MGLToolsPckgs/AutoDockTools/Utilities24/"
+PDBAA_PATH = "~/GASSER/database/pdbaa/pdbaa"
+MODELLER_PATH = "/opt/modeller923/bin/mod9.23"
+BRENDA_PATH = "../database/enzyme/brenda-90"
